98fdc68512be6fb217f34c1bc4a0acd7e0149575
ArturM2005 <arturmuhamediarov@gmail.com>
2024-07-06 17:52:37 +0400

    created app

---
+++ resources/views/Components/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home page</title>
+</head>
+<body>
+    {{$slot}}
+    <nav>
+        <ul>
+            <li>A</li>
+            <li>B</li>
+            <li>C</li>
+            <li>D</li>
+            <li>E</li>
+        </ul>
+    </nav>
+</body>
+</html>
